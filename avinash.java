7c583ba5a5c0be6992682cbb89c3c37bb25d0702
avinashantony <avinashantony093@gmail.com>
2025-06-25 15:23:25 +0530

    Add files via upload

---
+++ avinash.java
@@ -0,0 +1,28 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        int a=2;int b=4;
+        System.out.println(b%a);
+        System.out.println(b/a);
+        System.out.println(a*b);
+        System.out.println(a+b);
+        System.out.println(b-a);
+    }
+}
+
+
+
+Relation-------------------------------------
+class Main {
+    public static void main(String[] args) {
+        int a=2;int b=4;
+        System.out.println(b==a);
+        System.out.println(b!=a);
+        System.out.println(a<b);
+        System.out.println(a>b);
+        System.out.println(b>=a);
+        System.out.println(b<=a);
+    }
+}
