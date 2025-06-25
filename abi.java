6eb9cb79a46ed7a5573218d38d70f0b61a6ab6a0
avinashantony <avinashantony093@gmail.com>
2025-06-25 15:28:42 +0530

    Add files via upload

---
+++ abi.java
@@ -0,0 +1,52 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        int a=2;int b=4;
+        System.out.println(b%a);
+        System.out.println(b/a);
+        System.out.println(a*b);
+        System.out.println(a+b);
+        System.out.println(b-a);
+    }
+}
+
+
+
+Relation---------------------------------------------------------------------------------
+class Main {
+    public static void main(String[] args) {
+        int a=2;int b=4;
+        System.out.println(b==a);
+        System.out.println(b!=a);
+        System.out.println(a<b);
+        System.out.println(a>b);
+        System.out.println(b>=a);
+        System.out.println(b<=a);
+    }
+}
+assessment----------------------------------------------------------------------------------
+
+class Main {
+    public static void main(String[] args) {
+        int a=2;
+        System.out.println(a+=5);
+        System.out.println(a-=5);
+        System.out.println(a*=5);
+        System.out.println(a/=4);
+        System.out.println(a%=5);
+        
+    }
+}
+unarry-------------------------------------------------------------------------------------
+class Main {
+    public static void main(String[] args) {
+        int a=2;
+        System.out.println(a++);
+        System.out.println(a);
+       System.out.println(a--); 
+       System.out.println(a);
+        
+    }
+}
