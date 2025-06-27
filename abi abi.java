451778d440a84171a8290a9a505ee834bced317e
avinashantony <avinashantony093@gmail.com>
2025-06-27 10:57:22 +0530

    Add files via upload

---
+++ abi abi.java
@@ -0,0 +1,51 @@
+class Main {
+  
+  public static void main(String[] args) {
+   
+     int num=7;
+     
+   System.out.println("number is"+num);
+   
+     int a=num++;
+     
+   System.out.println("a is"+a);
+   
+     System.out.println("number is"+num);
+   
+     int b=++num;
+   
+     System.out.println("b is"+b);
+    
+    System.out.println("number is"+num);
+ 
+   }
+
+}
+
+
+
+
+
+____________________________________________________________________________________________________________________________
+class Main {
+  
+  public static void main(String[] args) {
+   
+     boolean isjavafun=true;
+     
+   boolean isFishtaste=false;
+    
+    System.out.println(isjavafun);
+   
+     System.out.println
+      int x=10;
+  
+      int y=9;
+ 
+       System.out.println(x>y);
+   
+     System.out.println(10>9);
+  
+  }
+
+}
